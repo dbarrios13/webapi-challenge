9d3cf71791f6de09f06ca49595002dadb408f40b
Lambda_School_Loaner_121 <lambda_school_loaner_121@Lambda-School-Loaner-121s-MacBook-Pro.local>
2019-06-07 10:43:07 -0700

    added requests for actions working on post action request

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -2,6 +2,21 @@ const express = require('express')
 const db = require('./actionModel')
 const router = express.Router()
 
-
+router.get('/:id', async (req, res) => {
+    try {
+        const action = await db.get(req.params.id)
+        if(action) {
+            res.status(200).json(action)
+        } else {
+            res.status(404).json({
+                message: 'The action with the specified ID does not exist'
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error retrieving actions'
+        })
+    }
+})
 
 module.exports = router
--- data/helpers/projectRouter.js
+++ data/helpers/projectRouter.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const db = require('./projectModel')
+const actionsDb = require('./actionModel')
 const router = express.Router()
 
 router.get('/:id', async(req, res) => {
@@ -51,6 +52,65 @@ router.delete('/:id', async(req, res) => {
     try {
         const project = await db.remove(req.params.id)
         if(project) {
+            res.status(204).json({
+                message: 'The project has been deleted'
+            })
+        } else {
+            res.status(404).json({
+                message: 'The project with the specified ID does not exist'
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error deleting the project'
+        })
+    }
+})
+
+router.get('/:id/actions/:id', async(req, res) => {
+    try {
+        const actions = await actionsDb.get(req.params.id)
+        if (actions) {
+            res.status(200).json(actions)
+        } else {
+            res.status(404).json({
+                message: 'The actions for the project with the specified ID does not exist'
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error retrieving project actions'
+        })
+    }
+})
+
+router.post('/:id/actions', async (req, res) => {
+    try {
+        const action = await actionsDb.insert(req.body)
+        res.status(201).json(action)
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error adding the project',
+            error: req.body
+        })
+    }
+})
+
+router.put('/:id/actions/:id', async(req, res) => {
+    try {
+        const action = await actionsDb.update(req.params.id, req.body)
+        res.status(200).json(action)
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error updating the project'
+        })
+    }
+})
+
+router.delete('/:id/actions/:id', async(req, res) => {
+    try {
+        const action = await db.remove(req.params.id)
+        if(action) {
             res.status(204).json({
                 message: 'The project has been deleted'
             })
