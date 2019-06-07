c4cf8c7817387c3891c6003f065690034a95a373
Lambda_School_Loaner_121 <lambda_school_loaner_121@Lambda-School-Loaner-121s-MacBook-Pro.local>
2019-06-07 10:17:33 -0700

    put and delete request created for project

--- data/helpers/projectRouter.js
+++ data/helpers/projectRouter.js
@@ -36,4 +36,34 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.put('/:id', async(req, res) => {
+    try {
+        const project = await db.update(req.params.id, req.body)
+        res.status(200).json(project)
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error updating the project'
+        })
+    }
+})
+
+router.delete('/:id', async(req, res) => {
+    try {
+        const project = await db.remove(req.params.id)
+        if(project) {
+            res.status(204).json({
+                message: 'The project has been deleted'
+            })
+        } else {
+            res.status(404).json({
+                message: 'The project with the specified ID does not exist'
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error deleting the project'
+        })
+    }
+})
+
 module.exports = router
